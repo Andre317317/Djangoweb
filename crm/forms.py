a383d6a4f656914def3829dd26340328d5dc2076
YOUR_NAME <YOUR_EMAIL>
2022-08-28 17:56:32 +0400

    second_comit

---
+++ crm/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+class OrderForm(forms.Form):
+    name = forms.CharField(max_length=200, label=('Имя'), widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    phone = forms.CharField(max_length=200, label=('Телефон'), widget=forms.TextInput(attrs={'class' : 'form-control'}))
--- landingpagewebsite/urls.py
+++ landingpagewebsite/urls.py
@@ -19,10 +19,15 @@ from django.urls import path
 from django.contrib import admin
 from django.urls import path
 from crm import views
+from django.conf.urls.static import static
+from django.conf import settings
+
 #from .views import first_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.first_page)
+    path('', views.first_page),
+    path('thanks/', views.thanks_page, name = 'thanks_page')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 #    path('', first_page)
-]
+
